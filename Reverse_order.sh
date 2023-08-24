87f57b0f87fc00885198b2d1b6c33f2daeb4acde
mahesh <maheshthota2136@gmail.com>
2023-08-24 11:33:21 +0530

    Updated

--- Reverse_order.sh
+++ Reverse_order.sh
@@ -4,13 +4,13 @@ rm -f replaced.txt
 
 echo "Before reversing the order"
 
-echo "--------------------------- \\n"
+echo "---------------------------"
 
 cat test_replace.txt
 
 rev test_replace.txt &>> replaced.txt 
 
-echo "After reversing \\n"
+echo "After reversing"
 
 echo "----------------"
 
